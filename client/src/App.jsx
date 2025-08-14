eb96f504064551ba582743e648f29602032c7ccb
sujit kumar <kumardj0846@gmail.com>
2025-08-14 17:19:50 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import { AuthContext } from "../context/AuthContext";
 const App = () => {
   const { authUser } = useContext(AuthContext);
   return (
-    <div className="bg-[url('./bg.png')] bg-contain ">
+    <div className="bg-[url('/bg.png')] bg-contain ">
       <Toaster />
       <Routes>
         <Route
